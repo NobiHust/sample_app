dda62ac9ce53059696d63df456edef03f9f282d8
NobiHust <nobihust@gmail.com>
2018-07-16 13:33:11 +0700

    chapter 11: Account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.select(:name, :email, :id, :admin).order(:name)
-                                  .page(params[:page])
+                 .page(params[:page]).per(Settings.per_page)
   end
 
   def new
@@ -16,9 +16,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t "welcome"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = t("please_check_email")
+      redirect_to root_url
     else
       render :new
     end
